70131afe32a266fe6354ba9a85bcfad0c502f008
choorpie <choorpie@gmail.com>
2021-04-07 00:18:01 +0800

    忘了存檔

--- ch05/src/cc/openhome/CashCard.java
+++ ch05/src/cc/openhome/CashCard.java
@@ -10,4 +10,37 @@ class CashCard {
 		this.balance = balance;
 		this.bonus = bonus;
 	}
+	
+	void store(int money) { // 儲值時呼叫的方法
+		if(money>0) {
+			this.balance += money;
+			if(money>=1000) {
+				this.bonus++;
+			}
+		}
+		else {
+			System.out.println("儲值是負的？你來亂的嗎？");
+		}
+	}
+	
+	void charge(int money) { // 扣款時呼叫的方法
+		if(money>0) {
+			if(money<=this.balance) {
+				this.balance-=money;
+			}
+			else {
+				System.out.println("錢不夠啦！");
+			}
+			
+		} else {
+			System.out.println("扣負數？這是在叫我儲值嗎？");
+		}	
+	}
+	
+	int exchange(int bonus) { // 兌換紅利點數時呼叫的方法
+		if(bonus>0) {
+			this.bonus-=bonus;
+		}
+		return this.bonus;
+	}
 }
